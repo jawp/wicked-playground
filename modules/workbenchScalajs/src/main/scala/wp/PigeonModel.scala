59cd7487d5183d6b4688ef347d07d45d213da478
pawel.panasewicz <pawel.panasewicz@gmail.com>
2016-09-25 20:15:57 +0200

    extracted class to separate file

---
+++ modules/workbenchScalajs/src/main/scala/wp/PigeonModel.scala
@@ -0,0 +1,38 @@
+package wp
+
+
+object PigeonModel {
+
+  sealed trait Status
+  case object Landing extends Status
+  case object Starting extends Status
+  case class Flying(action: Action) extends Status
+  case class Standing(action: Action) extends Status
+
+  sealed trait Action
+  case object Pecking extends Action //dziobać
+  case object Shitting extends Action
+
+  case class Pigeon(name: String, age: Int, status: Status)
+  type PigeonId = Int
+
+  var pigeons = Map[PigeonId, Pigeon](
+    1 -> Pigeon("Lucy", 1, Landing),
+    2 -> Pigeon("Bob", 2, Flying(Shitting)),
+    3 -> Pigeon("Carmen", 2, Starting),
+    4 -> Pigeon("Graham", 2, Standing(Pecking)),
+    5 -> Pigeon("All", 2, Landing)
+  )
+
+  def nextId(): PigeonId = pigeons.keySet.max + 1
+
+  def addPigeon(pigeon: Pigeon): PigeonId = {
+    val id = nextId()
+    pigeons  = pigeons + (id -> pigeon)
+    id
+  }
+
+  def removePigeon(): Unit = {
+    pigeons = pigeons.tail
+  }
+}
