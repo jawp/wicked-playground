28325135e90cb11cc30b5787c422931f1844eef5
pawel.panasewicz <pawel.panasewicz@gmail.com>
2016-09-27 19:36:27 +0200

    cleanup

--- modules/workbenchScalajs/src/test/scala/wp/ScalaRxDemo.scala
+++ modules/workbenchScalajs/src/test/scala/wp/ScalaRxDemo.scala
@@ -3,6 +3,9 @@ package wp
 import rx._
 import rx.Ctx.Owner
 
+import scala.collection.mutable
+import scala.util.{Failure, Success}
+
 class ScalaRxDemo extends WickedSpec {
 
   "rx and Ctx.Owner macro magic" in {
@@ -31,6 +34,113 @@ class ScalaRxDemo extends WickedSpec {
         a() + b()
       }
     }
+  }
+
+  "more examples on Ctx.Owner" in {
+
+    import Ctx.Owner.Unsafe._
+
+    var count = 0
+    val a = Var(1);
+    val b = Var(2)
+
+    def mkRx(i: Int)(implicit ctx: Ctx.Owner) = Rx { count += 1; i + b() }
+
+    def mkC(implicit ctx: Ctx.Owner) = Rx {
+      val newRx = mkRx(a())
+      newRx()
+    }
+
+    val c = mkC
+
+    println(c.now,count) // (3,1)
+    a() = 4
+    println(c.now,count) // (6,2)
+    b() = 3
+    println(c.now,count) // (7,4)
+    (0 to 100).foreach { i => a() = i }
+    println(c.now,count) //(103,105)
+    b() = 4
+    println(c.now,count) //(104,107)
+
+
+    def updateA (f: Int => Int)(implicit ctx: Ctx.Owner): Unit = {
+      a() = f(a.now)
+    }
+  }
+
+  import mutable.{MutableList => mList}
+  import mutable.{Map => mMap}
+
+  "simple Rx" in {
+    import Ctx.Owner.Unsafe._
+    val log = mList[Symbol]()
+    val empty = mList[Symbol]()
+    val x = Var{1}
+    x.triggerLater(
+      log.+=('xChanged)
+    )
+    log mustBe empty
+    x.now mustBe 1
+    x() = 12
+    log mustBe mList('xChanged)
+    x.now mustBe 12
+  }
+
+  "higher ordered rxes" in {
+    import Ctx.Owner.Unsafe._
+    val log = mList[Symbol]()
+    val empty = mList[Symbol]()
+    val xA = Var(1)
+    xA.triggerLater(
+      log.+=('xAChanged)
+    )
+    val xB = Var(2)
+    xB.triggerLater(
+      log.+=('xBChanged)
+    )
+    val xs = Var {
+      List(
+        xA,
+        xB
+      )
+    }
+    xs.triggerLater(
+      log.+=('xsChanged)
+    )
+    log mustBe empty
+    xA() = 10
+    xA.now mustBe 10
+    log mustBe mList('xAChanged)  //outer RX is not fired
+    xs.now.map(_.now) mustBe mList(10,2) //but inner Var have been changed
+  }
+
+  "higher ordered rxes - Rx instead of Var" in {
+    import Ctx.Owner.Unsafe._
+    val log = mList[Symbol]()
+    val empty = mList[Symbol]()
+    val xA = Var(1)
+    xA.triggerLater(
+      log.+=('xAChanged)
+    )
+    val xB = Var(2)
+    xB.triggerLater(
+      log.+=('xBChanged)
+    )
+    val xs = Var {
+      List(
+        Rx{xA()+1},
+        xB
+      )
+    }
+    xs.triggerLater(
+      log.+=('xsChanged)
+    )
+    log mustBe empty
+    xA() = 10
+    xA.now mustBe 10
+    log mustBe mList('xAChanged)  //outer RX is not fired
+    xs.now.map(_.now) mustBe mList(11,2) //but inner Var have been changed
   }
 
 }
