d710db771ed23b621f5ff0a3d1f57ede4372888d
pawel.panasewicz <pawel.panasewicz@gmail.com>
2016-10-17 13:32:29 +0200

    organized imports

--- modules/testGoodies/src/test/scala/wp/Discipline.scala
+++ modules/testGoodies/src/test/scala/wp/Discipline.scala
@@ -1,7 +1,6 @@
 package wp
 
 import org.scalatest.FunSuite
-import org.typelevel.discipline.scalatest.Discipline
 
 class Discipline extends FunSuite with org.typelevel.discipline.scalatest.Discipline
 
