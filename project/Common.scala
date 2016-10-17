92f763a1dcfc35a2a6ebffe8717eace7020846da
pawel.panasewicz <pawel.panasewicz@gmail.com>
2016-10-17 13:00:10 +0200

    fine graned scalac options

--- project/Common.scala
+++ project/Common.scala
@@ -8,7 +8,19 @@ object Common {
     version := appVersion,
     scalaVersion := "2.11.8",
     organization := "jawp",
-    scalacOptions ++= Seq("-unchecked", "-deprecation", "-feature", "-encoding", "utf8"),
+    scalacOptions ++= Seq( //based on http://blog.threatstack.com/useful-scalac-options-for-better-scala-development-part-1
+      "-target:jvm-1.8",
+      "-encoding", "UTF-8",
+      "-unchecked",
+      "-deprecation",
+      "-feature",
+      "-Xfuture",
+//      "-Yno-adapted-args",
+      "-Ywarn-dead-code",
+      "-Ywarn-numeric-widen",
+      "-Ywarn-value-discard",
+      "-Ywarn-unused"
+    ),
     testOptions in Test += Tests.Argument("-oD", "-F", "10")   //org.scalatest.tools.Runner - description of options
   )
 }
