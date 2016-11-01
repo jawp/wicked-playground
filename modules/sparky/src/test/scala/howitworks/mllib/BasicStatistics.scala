706925266d99d8f5e4f0c4c25e89db2c6dddac53
pawel.panasewicz <pawel.panasewicz@gmail.com>
2016-11-01 11:56:23 +0100

    basic correlations

--- modules/sparky/src/test/scala/howitworks/mllib/BasicStatistics.scala
+++ modules/sparky/src/test/scala/howitworks/mllib/BasicStatistics.scala
@@ -4,12 +4,17 @@ package howitworks.mllib
 import java.lang.Math._
 
 import com.holdenkarau.spark.testing.{SharedSparkContext, SparkContextProvider}
-import org.apache.spark.mllib.linalg.{Vector, Vectors}
+import org.apache.spark.mllib.linalg.{Matrices, Matrix, Vector, Vectors}
 import org.apache.spark.rdd.RDD
 
 import scalaz.syntax.id._
 
+
+//see more examples in the Spark repo at examples/src/main/scala/org/apache/spark/examples/mllib/SummaryStatisticsExample.scala
+
 class BasicStatistics extends wp.Spec with SharedSparkContext {
+
+  import org.apache.spark.mllib.stat._
 
   "Summary Stats" in {
     val observations: RDD[Vector] = sc.parallelize(Seq(
@@ -19,7 +24,6 @@ class BasicStatistics extends wp.Spec with SharedSparkContext {
     ))
 
     //these stats are computed COLUMN WISE
-    import org.apache.spark.mllib.stat._
     val summaryStats = Statistics.colStats(observations)
 
     summaryStats.mean mustBe Vectors.dense(2.0, 20.0, 200.0)
@@ -31,4 +35,42 @@ class BasicStatistics extends wp.Spec with SharedSparkContext {
     summaryStats.normL1 mustBe Vectors.dense(6.0, 60.0, 600.0) //http://mathworld.wolfram.com/L1-Norm.html
     summaryStats.normL2 mustBe Vectors.dense(sqrt(14.0), sqrt(100+400+900), sqrt(10000 + 40000 + 90000)) //http://mathworld.wolfram.com/L2-Norm.html
   }
+
+  "Correlations" in {
+    val seriesX: RDD[Double] = sc.parallelize(Array(1, 2, 3, 3, 5))
+    // series
+    // must have the same number of partitions and cardinality as seriesX
+    val seriesY: RDD[Double] = sc.parallelize(Array(11, 22, 33, 33, 555))
+
+    val correlation: Double = Statistics.corr(seriesX, seriesY, "pearson")
+    correlation mustBe 0.8500286768773001
+
+    Statistics.corr(seriesX, seriesX, "pearson") mustBe 1.0000000000000002 //embarassed longs ...
+    Statistics.corr(seriesX, seriesX.map(_*2), "pearson") mustBe 1.0000000000000002 //embarassed longs ...
+
+    //above can be thought as computing correlation between two features
+    //each series can be thought as column in matrix representing observations
+
+    //now correlations between every two series from the set
+    //each serie is given by column in matrix
+
+    val data: RDD[Vector] = sc.parallelize(
+      Seq(
+        Vectors.dense(1.0, 10.0, 100.0),
+        Vectors.dense(2.0, 20.0, 200.0),
+        Vectors.dense(5.0, 33.0, 366.0))
+    )  // note that each Vector is a row and not a column
+
+    // calculate the correlation matrix using Pearson's method. Use "spearman" for Spearman's method
+    // If a method is not specified, Pearson's method will be used by default.
+    val correlMatrix: Matrix = Statistics.corr(data, "pearson")
+
+    correlMatrix mustBe Matrices.dense(3, 3, Array(
+      1,                  0.978883465889473,  0.9903895695275671, //first column
+      0.978883465889473,  1,                  0.9977483233986101, //second column
+      0.9903895695275671, 0.9977483233986101, 1                   //third column
+    ))
+  }
+
+
 }
